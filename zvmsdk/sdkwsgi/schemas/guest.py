e41cb87a1ebb6e71066c9ff6ff5199a658b1bfc8
QingFeng Hao <haoqf@linux.vnet.ibm.com>
2023-09-12 04:39:17 -0500

    Fix the problem of argument active in guest_create_network_interface
    Signed-off-by: QingFeng Hao <haoqf@linux.vnet.ibm.com>

--- zvmsdk/sdkwsgi/schemas/guest.py
+++ zvmsdk/sdkwsgi/schemas/guest.py
@@ -95,7 +95,7 @@ create_network_interface = {
                 'guest_networks': parameter_types.network_list,
                 'active': parameter_types.boolean,
             },
-            'required': ['os_version', 'guest_networks'],
+            'required': ['os_version', 'guest_networks', 'active'],
             'additionalProperties': False,
         },
         'additionalProperties': False,
